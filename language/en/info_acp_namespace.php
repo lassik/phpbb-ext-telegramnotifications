8a49df3887f8e4d63b400ddc5b62e60bb656a3f6
Lassi Kortela <lassi@lassi.io>
2018-01-30 18:16:22 +0200

    Make ACP pages slightly more conventional

--- language/en/info_acp_namespace.php
+++ language/en/info_acp_namespace.php
@@ -18,14 +18,15 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_TELEGRAM_BOT_AUTH_TOKEN'   => 'Telegram bot auth token',
-	'ACP_TELEGRAM_CHAT_ID'          => 'Telegram chat ID',
-	'ACP_TELEGRAM_ERROR'            => 'Error message',
-	'ACP_TELEGRAM_FIND_CHAT_ID'     => 'Find chat ID',
-	'ACP_TELEGRAM_FOUND_CHAT_ID'    => 'Found the following chat ID',
-	'ACP_TELEGRAM_LAST_ERROR'       => 'Last error',
-	'ACP_TELEGRAM_NOTIFICATIONS'    => 'Telegram Notifications',
-	'ACP_TELEGRAM_SETTINGS'         => 'Telegram settings',
+	'ACP_TELEGRAM_BOT_AUTH_TOKEN'	=> 'Telegram bot auth token',
+	'ACP_TELEGRAM_CHAT_ID'			=> 'Telegram chat ID',
+	'ACP_TELEGRAM_CONNECTION'		=> 'Telegram connection',
+	'ACP_TELEGRAM_ERROR'			=> 'Error message',
+	'ACP_TELEGRAM_FIND_CHAT_ID'		=> 'Find chat ID',
+	'ACP_TELEGRAM_FOUND_CHAT_ID'	=> 'Found the following chat ID',
+	'ACP_TELEGRAM_LAST_ERROR'		=> 'Last error',
+	'ACP_TELEGRAM_NOTIFICATIONS'	=> 'Telegram Notifications',
+	'ACP_TELEGRAM_SETTINGS'			=> 'Telegram settings',
 	'ACP_TELEGRAM_SETTINGS_UPDATED'	=> 'Telegram settings have been updated',
-	'ACP_TELEGRAM_USE_CHAT_ID'      => 'Use this chat ID',
+	'ACP_TELEGRAM_USE_CHAT_ID'		=> 'Use this chat ID',
 ));
